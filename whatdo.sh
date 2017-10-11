a33023ed074f24a832582e00b04e23833660633d
sharlak <sharla.kew@riversafe.co.uk>
2017-10-11 17:29:39 +0100

    bash script

---
+++ whatdo.sh
@@ -0,0 +1,6 @@
+####### setup phase #######
+# create ~/.whatdo if it does not exist
+# create ~/.whatdo/whatdoing if not exist and populate with "nothing"
+# create ~/.whatdo/whatdid if not exist add placeholder
+
+# BUT FIRST with manually created files
